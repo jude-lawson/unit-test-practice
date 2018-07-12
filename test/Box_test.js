9ec887004582ce86517f8963a7110d0284ee88b0
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-12 12:46:11 -0600

    Tests Box's attributes

--- test/Box_test.js
+++ test/Box_test.js
@@ -1,7 +1,26 @@
 const chai = require('chai');
 const assert = chai.assert;
+const Box = require('../Box.js')
+
 describe('Box', () => {
   it('should return true', () => {
     assert(true);
   });
+
+  it('should have a default height and width', () => {
+    let box = new Box();
+
+    assert.equal(box.height, 100);
+    assert.equal(box.width, 100);
+  });
+
+  it('should take a height and a width as arguments', () => {
+    let box = new Box(50, 40);
+
+    assert.equal(box.height, 50);
+    assert.equal(box.width, 40);
+  });
+
+
+  
 });
