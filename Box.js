4accd683692f91f1b834634eb4b5ad33c3d44665
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-12 13:09:25 -0600

    Moves area function directly into Box object definition

--- Box.js
+++ Box.js
@@ -1,10 +1,13 @@
 function Box(height = 100, width = 100) {
   this.height = height,
-  this.width = width
+  this.width = width,
+  this.area = function() {
+    return this.height * this.width;
+  }
 }
 
-Box.prototype.area = function() {
-  return this.height * this.width;
-}
+// Box.prototype.area = function() {
+//   return this.height * this.width;
+// }
 
 module.exports = Box;
